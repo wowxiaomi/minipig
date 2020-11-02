a22111609d3bdc07ad9d77eceaecc7404e402d34
mamabooktemp <suncomeon@hotmail.com>
2020-11-02 16:50:07 +0800

    搭建完成微信框架v0.1

---
+++ src/main/java/com/minipig/MinipigApplication.java
@@ -0,0 +1,13 @@
+package com.minipig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinipigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinipigApplication.class, args);
+    }
+
+}
